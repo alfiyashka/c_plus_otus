5ab2b82d6bbc429755d77cbf8575261dc51fdf18
alfiya valieva <v-alfiya@mail.ru>
2024-09-02 20:21:44 +0300

    add packaging

--- hm1/tests/hw1BoostTest.cpp
+++ hm1/tests/hw1BoostTest.cpp
@@ -9,8 +9,9 @@ BOOST_AUTO_TEST_SUITE( check_version_test )
     BOOST_AUTO_TEST_CASE( version_test )
     {
         const std::string version(OTUS_HW1_VERSION);
-        std::cout<<version.c_str() << std::endl;
+        std::cout<<"Version " <<version.c_str() << std::endl;
         BOOST_CHECK( !version.empty() );
     }
 
 BOOST_AUTO_TEST_SUITE_END()
+
