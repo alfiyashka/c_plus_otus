34435f404cc53aea6bfa9e91a1ceb33f716939f8
alfiyashka <33715889+alfiyashka@users.noreply.github.com>
2024-09-02 20:40:15 +0300

    Update functions.cmake

--- hm1/functions.cmake
+++ hm1/functions.cmake
@@ -1,5 +1,12 @@
 function(generate_packages NAME)
-    
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libpurple0 (>= ${PURPLE_VERSION})") # TODO: libglib2.0-0 could be added here, but it is non-trivial to do. libpurple0 depends on it anyway, so we should be good.
+    if (${OPUSFILE_FOUND})
+        set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS}, libopusfile0 (>= ${OPUSFILE_VERSION})")
+    endif()
+    if ("${PIXBUF_FOUND}")
+        set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS}, libgdk-pixbuf-2.0-0 (>= ${PIXBUF_VERSION})")
+    endif()
     set(CPACK_GENERATOR "DEB")
 
     set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
